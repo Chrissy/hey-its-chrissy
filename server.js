10859c96696d06bbe5e212ea59b9e94d57e794df
chrissy <christopherchristopherclark@gmail.com>
2024-06-03 13:23:35 -0700

    automatically switch to s3 in production

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ if (process.env.NODE_ENV == 'production') {
   app.get('/dist/bundle.js', (request, response) => {
     response.redirect('https://s3-us-west-2.amazonaws.com/chrissy-portfolio-images/bundle.js');
   })
+
+  app.get('/images/*', (request, response) => {
+    response.redirect(`https://s3-us-west-2.amazonaws.com/chrissy-portfolio-images/${request.params['0']}`);
+  })
 }
 
 app.use(express.static('static'));
